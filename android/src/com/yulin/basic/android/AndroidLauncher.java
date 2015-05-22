866b4bea2dc6f2bf6c48ec2db7b1c312b61d4c7f
YulinAle <smoran0990@gmail.com>
2015-05-22 01:38:59 -0500

    Initial

---
+++ android/src/com/yulin/basic/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.yulin.basic.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.yulin.basic.Basic;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Basic(), config);
+	}
+}
---
+++ desktop/src/com/yulin/basic/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.yulin.basic.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.yulin.basic.Basic;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Basic(), config);
+	}
+}
