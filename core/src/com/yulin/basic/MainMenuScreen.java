ad25d12a75b4726415bef0db5be25bb2af034f65
YulinAle <smoran0990@gmail.com>
2015-05-24 21:42:52 -0500

    Implemented basic Screen changing

---
+++ core/src/com/yulin/basic/MainMenuScreen.java
@@ -0,0 +1,61 @@
+package com.yulin.basic;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+
+public class MainMenuScreen implements Screen {
+    final Basic game;
+    OrthographicCamera camera;
+
+    public MainMenuScreen(final Basic gam){
+        game=gam;
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false,1280,720);
+
+    }
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(1,1,1,1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        camera.update();
+
+        if(Gdx.input.isTouched()){
+            game.setScreen(new LevelScreen(game));
+            dispose();
+        }
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
